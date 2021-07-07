b32d1f1f41c3ea1ee5bc08e748aca390e06bf682
Ivana Parrilla <ip22@njit.edu>
2021-07-06 22:50:28 -0400

    Feature: Added Multiplication Method to Calculator

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -1,4 +1,3 @@
-
 import unittest
 from Calculator import  Calculator
 from CSVReader import CsvReader
@@ -25,6 +24,14 @@ class MyTestCase(unittest.TestCase):
         for row in test_data:
             self.assertEqual(self.calculator.subtract(row['Value 1'],row['Value 2']),int(row['Result']))
             self.assertEqual(self.calculator.result,int(row['Result']))
+        test_data.clear()
+
+    def test_multiplication_method_calculator(self):
+        test_data = CsvReader('/src/Unit Test Multiplication.csv').data
+        #pprint(test_data)
+        for row in test_data:
+            self.assertEqual(self.calculator.multiply(row['Value 1'],row['Value 2']),float(row['Result']))
+            self.assertEqual(self.calculator.result,float(row['Result']))
         test_data.clear()
 
 if __name__ == '__main__':
